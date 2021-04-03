68444341b540710fddb040a10e699ff7ab3e6b4d
annikaverma <76493450+annikaverma@users.noreply.github.com>
2021-04-03 19:37:43 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function take_snapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/YwHvb2xU8/',modelLoaded);
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/YwHvb2xU8/model.json',modelLoaded);
 
   function modelLoaded() {
     console.log('Model Loaded!');
